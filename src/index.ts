5eb2a5a6d04f7fcc4a15a4999ce0f8a63428aab1
Aaron Luman <aaron.luman@gmail.com>
2024-02-06 14:02:55 -0800

    dix docker and setup watch

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-    res.json({hello: 'world'});
+    res.json({hello: 'guys'});
 });
 
 const port = Number(process.env.PORT || 8080);
