98eda661679302ed0a626b8e10318974e1094ba8
Aaron Luman <aaron.luman@gmail.com>
2024-02-06 13:35:30 -0800

    init express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('hello');
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({hello: 'world'});
+});
+
+const port = Number(process.env.PORT || 8080);
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server listening at localhost:${port}`);
+});
